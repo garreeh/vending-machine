080ea2b3478ee8a52f36aeacea66145906e3bdf2
Garry Gajultos <gajultos.garry123@gmail.com>
2026-08-07 19:44:07 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vending-machine',
+  webDir: 'dist'
+};
+
+export default config;
